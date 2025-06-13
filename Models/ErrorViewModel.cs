ff79b5557efe0188bcfab5fb34d13e6d350ab50a
Tatva-MeetJadav <tatva.pct169@outlook.com>
2025-06-13 18:31:59 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MicroservicesUser.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
